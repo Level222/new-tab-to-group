3bb53351edfdd5548380f309cb0edb3cb36330aa
Level222 <124219660+Level222@users.noreply.github.com>
2023-04-30 22:44:32 +0900

    開いたタブを削除しとき前開いていたタブを開くようにした。

--- background.js
+++ background.js
@@ -1,22 +1,63 @@
-const openTabToGroup = async (groupId) => {
-  const groupTabs = await chrome.tabs.query({ groupId });
-  const groupEndTabIndex = Math.max(...groupTabs.map((tab) => tab.index));
+"use strict";
 
-  const newTab = await chrome.tabs.create({ index: groupEndTabIndex + 1 });
+const getActiveTab = async () => (await chrome.tabs.query({ active: true, lastFocusedWindow: true }))[0];
 
-  await chrome.tabs.group({
-    groupId,
-    tabIds: [newTab.id]
-  });
+class OpenedTab {
+  #tabId;
+  #lastFocusedTabId;
 
-  return newTab;
-};
+  constructor(tabId, lastFocusedTabId) {
+    this.#tabId = tabId;
+    this.#lastFocusedTabId = lastFocusedTabId;
+
+    chrome.tabs.onActivated.addListener(this.#handleActivate);
+    chrome.tabs.onRemoved.addListener(this.#handleRemove);
+  }
+
+  #clearListeners() {
+    chrome.tabs.onActivated.removeListener(this.#handleActivate);
+    chrome.tabs.onRemoved.removeListener(this.#handleRemove);
+  }
+
+  #handleActivate = () => {
+    this.#clearListeners();
+  };
+
+  #handleRemove = (tabId) => {
+    if (tabId === this.#tabId) {
+      chrome.tabs.update(this.#lastFocusedTabId, { active: true });
+    }
+
+    this.#clearListeners();
+  }
+
+  static async createToGroup(groupId) {
+    const groupTabs = await chrome.tabs.query({ groupId });
+    const groupEndTabIndex = Math.max(...groupTabs.map((tab) => tab.index));
+
+    const lastFocusedTab = await getActiveTab();
+
+    const newTab = await chrome.tabs.create({ index: groupEndTabIndex + 1 });
+    const newTabId = newTab.id;
+
+    if (groupId === -1) {
+      return;
+    }
+
+    await chrome.tabs.group({
+      groupId,
+      tabIds: [newTabId]
+    });
+
+    return new OpenedTab(newTabId, lastFocusedTab.id);
+  }
+}
 
 const openTabToActiveGroup = async () => {
-  const [activeTab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  const activeTab = await getActiveTab();
   const activeGroupId = activeTab.groupId;
 
-  return await openTabToGroup(activeGroupId);
+  return await OpenedTab.createToGroup(activeGroupId);
 };
 
 chrome.commands.onCommand.addListener((command) => {
