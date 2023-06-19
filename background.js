90adddaa0587e706bd4162d976fa6e68b7514aeb
Level222 <124219660+Level222@users.noreply.github.com>
2023-06-19 22:40:50 +0900

    コード整理

--- background.js
+++ background.js
@@ -3,6 +3,25 @@
 const getActiveTab = async () => (await chrome.tabs.query({ active: true, lastFocusedWindow: true }))[0];
 
 class OpenedTab {
+  static async createToGroup(groupId) {
+    const groupTabs = await chrome.tabs.query({ groupId });
+    const groupEndTabIndex = Math.max(...groupTabs.map((tab) => tab.index));
+
+    const lastFocusedTab = await getActiveTab();
+
+    const newTab = await chrome.tabs.create({ index: groupEndTabIndex + 1 });
+    const newTabId = newTab.id;
+
+    if (groupId !== -1) {
+      await chrome.tabs.group({
+        groupId,
+        tabIds: [newTabId]
+      });
+    }
+
+    return new OpenedTab(newTabId, lastFocusedTab.id);
+  }
+
   #tabId;
   #lastFocusedTabId;
 
@@ -29,26 +48,7 @@ class OpenedTab {
     }
 
     this.#clearListeners();
-  }
-
-  static async createToGroup(groupId) {
-    const groupTabs = await chrome.tabs.query({ groupId });
-    const groupEndTabIndex = Math.max(...groupTabs.map((tab) => tab.index));
-
-    const lastFocusedTab = await getActiveTab();
-
-    const newTab = await chrome.tabs.create({ index: groupEndTabIndex + 1 });
-    const newTabId = newTab.id;
-
-    if (groupId !== -1) {
-      await chrome.tabs.group({
-        groupId,
-        tabIds: [newTabId]
-      });
-    }
-
-    return new OpenedTab(newTabId, lastFocusedTab.id);
-  }
+  };
 }
 
 const openTabToActiveGroup = async () => {
